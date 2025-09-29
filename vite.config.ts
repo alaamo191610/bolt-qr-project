f005570b9348eb410450c434993e469fd0ed52d5
alaa94x <31366902+alaa94x@users.noreply.github.com>
2025-09-29 23:29:18 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   server: {
+//     host: '0.0.0.0',  // This exposes the Vite server to your local network
+//     port: 5173,        // Optional: specify a custom port if necessary
+//   },
+// });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    host: '0.0.0.0',  // This exposes the Vite server to your local network
-    port: 5173,        // Optional: specify a custom port if necessary
-  },
-});
+  // replace REPO with your actual repo name
+  base: '/REPO/'
+})
