1d048f9f7f8aa1d1c98e6ab43b874742aa886ebb
alaa94x <31366902+alaa94x@users.noreply.github.com>
2025-10-05 23:35:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-//   server: {
-//     host: '0.0.0.0',  // This exposes the Vite server to your local network
-//     port: 5173,        // Optional: specify a custom port if necessary
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // replace REPO with your actual repo name
-  base: '/bolt-qr-project/'
-})
+  base: '/bolt-qr-project/', 
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',  // This exposes the Vite server to your local network
+    port: 5173,        // Optional: specify a custom port if necessary
+  },
+});
